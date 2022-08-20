227f8fa2dc30c09d70cc118c05a2a7a5c61690f2
Sarina Chang <sarinachang7@gmail.com>
2022-08-20 18:35:33 -0400

    notes saved in localstorage

--- extension/src/components/Notes.js
+++ extension/src/components/Notes.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Notepad.css';
 
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export default function Notes({ theme }) {
+  // save and retrieve notes from local storage
+  const notesStorage = localStorage.getItem('notes');
+  const [notes, setNotes] = useState(notesStorage);
+
   const [open, setOpen] = useState(false);
 
+  // chevron styling/animation
   const chevronRotate = open ? 'rotate(90deg)' : 'rotate(0)';
   const chevronTheme = theme === 'light' ? '#f2f2f2' : '#000';
+
+  function onChange(e) {
+    localStorage.setItem('notes', e.target.value);
+    setNotes(e.target.value);
+  }
 
   return (
     <div id='notepad'>
@@ -26,6 +36,8 @@ export default function Notes({ theme }) {
       {open && (
         <div className='note-area-container'>
           <textarea
+            onChange={onChange}
+            value={notes}
             className='note-area'
             placeholder='         *⁺‧͙˚*･༓ ☾　Write a note... ☽ ༓･*˚‧͙⁺*'
           ></textarea>
