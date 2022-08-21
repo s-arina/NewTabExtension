3f139765a6c9f750a5b7565baf3361d050c1b3ab
Sarina Chang <sarinachang7@gmail.com>
2022-08-21 01:29:44 -0400

    keep notepad open/closed on refresh

--- extension/src/components/Notes.js
+++ extension/src/components/Notes.js
@@ -1,27 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../css/Notepad.css';
 
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export default function Notes({ theme }) {
-  // save and retrieve notes from local storage
+  // save and retrieve notes from local storage & remember if notepad was open or not on refresh
   const notesStorage = localStorage.getItem('notes');
+  const notepadState = localStorage.getItem('notepad');
+
   const [notes, setNotes] = useState(notesStorage);
+  // parse the localStorage value, boolean is stored as string
+  const [open, setOpen] = useState(JSON.parse(notepadState));
 
-  const [open, setOpen] = useState(false);
-
-  // chevron styling/animation
+  // chevron/other styling
   const chevronRotate = open ? 'rotate(90deg)' : 'rotate(0)';
   const chevronTheme = theme === 'light' ? '#f2f2f2' : '#000';
+  const placeholder = '         *⁺‧͙˚*･༓ ☾　Write a note... ☽ ༓･*˚‧͙⁺*';
 
   function onChange(e) {
     localStorage.setItem('notes', e.target.value);
     setNotes(e.target.value);
   }
 
+  function notepad() {
+    localStorage.setItem('notepad', !open);
+    setOpen(!open);
+  }
+
   return (
     <div id='notepad'>
-      <div className='notes' onClick={() => setOpen(!open)}>
+      <div className='notes' onClick={() => notepad()}>
         <h3>Notes</h3>
         <div className='chevron'>
           <KeyboardArrowRightIcon
@@ -39,7 +47,7 @@ export default function Notes({ theme }) {
             onChange={onChange}
             value={notes}
             className='note-area'
-            placeholder='         *⁺‧͙˚*･༓ ☾　Write a note... ☽ ༓･*˚‧͙⁺*'
+            placeholder={placeholder}
           ></textarea>
         </div>
       )}
