8f1779488343550f9c96f29649afdb9c620bb296
Sarina Chang <sarinachang7@gmail.com>
2022-11-10 00:43:32 -0500

    aws lamba test

--- extension/src/components/Weather.js
+++ extension/src/components/Weather.js
@@ -9,8 +9,8 @@ export default function Weather({ theme }) {
   const getLat = window.localStorage.getItem('lat');
   const getLong = window.localStorage.getItem('long');
 
-  const apiKey = process.env.REACT_APP_OPENWEATHER;
-  const url = 'https://api.openweathermap.org/data/2.5/';
+  // const apiKey = process.env.REACT_APP_OPENWEATHER;
+  // const url = 'https://api.openweathermap.org/data/2.5/';
 
   const [lat, setLat] = useState(getLat || '');
   const [long, setLong] = useState(getLong || '');
@@ -18,13 +18,30 @@ export default function Weather({ theme }) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [unit, setUnit] = useState(true);
+  const [key, setKey] = useState('');
+  const [url, setUrl] = useState('');
 
   const RetryIconTheme = theme === 'light' ? '#f2f2f2' : '#000';
 
   useEffect(() => {
+    getKey();
     getCoords();
     fetchWeatherData();
-  }, [lat, long]);
+  }, [lat, long, url, key]);
+
+  // api key
+  const getKey = async () => {
+    try {
+      const { data } = await axios.get(
+        'https://18lhlaabdi.execute-api.us-east-2.amazonaws.com/default/apiKey'
+      );
+      setUrl(data.url);
+      setKey(data.key);
+    } catch (err) {
+      setError(true);
+      console.log('Error: API call failed.');
+    }
+  };
 
   // user position
   const getCoords = () => {
@@ -56,16 +73,20 @@ export default function Weather({ theme }) {
 
   // api call for forecast
   const fetchWeatherData = async () => {
-    try {
-      const { data } = await axios.get(
-        `${url}weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`
-      );
-      setWeatherData(data);
-    } catch (err) {
-      setError(true);
-      console.log('Error: Could not retrieve weather data. Please try again.');
-    } finally {
-      setLoading(false);
+    if (url && key) {
+      try {
+        const { data } = await axios.get(
+          `${url}weather?lat=${lat}&lon=${long}&appid=${key}&units=imperial`
+        );
+        setWeatherData(data);
+      } catch (err) {
+        setError(true);
+        console.log(
+          'Error: Could not retrieve weather data. Please try again.'
+        );
+      } finally {
+        setLoading(false);
+      }
     }
   };
 
